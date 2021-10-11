11ae5daf04705559a3271b09b440b4c6a4db407f
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2021-10-11 00:00:10 -0400

    thunderkit project slipstream
    my asshole

---
+++ Slipstream/Assets/Slipstream/Scripts/Items/Yellow/RottingPerforator.cs
@@ -0,0 +1,107 @@
+﻿/*using R2API;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Slipstream.Items.Yellow
+{
+    public static class RottingPerforator
+    {
+        //public static ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
+
+        public static void Init() //This runs whatever methods you need to for the item, such as language stuff, effects, setting up the item, etc. Init is ran in the Awake() method in the Main.cs file and every item you add you have to run Init() inside of Awake() in the Main.cs file or else it won't work.
+        {
+            //RottingPerforatorItem();
+            AddLanguageTokens();
+            Hooks();
+        }
+
+        private static void RottingPerforatorItem()
+        {
+            itemDef.name = "RottingPerforator"; //Internal name of the item
+            itemDef.tier = ItemTier.Boss; //Tier of the item, Tier1 = White. Tier2 = Green, Tier3 = Red. Lunar = Blue. Boss = Yellow
+            itemDef.pickupModelPrefab = SlipstreamPlugin.AssetBundle.LoadAsset<GameObject>("Assets/Import/RottingPerforator/rottingperforator.prefab"); //3D Model of the item in the world/Logbook
+            itemDef.pickupIconSprite = SlipstreamPlugin.AssetBundle.LoadAsset<Sprite>("Assets/Import/RottingPerforator/rottingperforator_icon.png"); //Sprite of the item  when in your inventory/Logbook
+            itemDef.nameToken = "ROTTINGPERFORATOR_NAME"; //Name of the item ingame
+            itemDef.pickupToken = "ROTTINGPERFORATOR_PICKUP"; //Text when you hover over the item in your inventoy or the tooltip when you pick it up
+            itemDef.descriptionToken = "ROTTINGPERFORATOR_DESC"; //Description of the item in the logbook (also I think what the tooltip is with ItemStats on?
+            itemDef.loreToken = "ROTTINGPERFORATOR_LORE"; //Lore of the item in the logbook
+            itemDef.tags = new ItemTag[] { ItemTag.Damage, ItemTag.OnKillEffect }; //Tags for the item, for stuff such as classifying it for Utility/Damage/Healing chests, if it's blacklisted from Mithrix, if it's scrap, if it's sprint related, etc.
+
+            //TODO: MAKE THIS SHIT ACTUALLY WORK
+            var itemDisplayRules = new ItemDisplayRule[1];
+            itemDisplayRules[0].followerPrefab = SlipstreamPlugin.AssetBundle.LoadAsset<GameObject>("Assets/Import/snake/snake.prefab");
+            itemDisplayRules[0].childName = "Chest";
+            itemDisplayRules[0].localScale = new Vector3(2f, 2f, 2f);
+
+            SlipstreamPlugin.ModItemDefs.Add(itemDef); //Adds this item to the list of itemDefs in Main.cs, which then gets added to the ContentPack which then gets added to the game's content
+        }
+
+        /*private static void AddLanguageTokens()
+        {
+            LanguageAPI.Add("ROTTINGPERFORATOR_NAME", "Rotting Perforator"); //Name of the item ingame
+            LanguageAPI.Add("ROTTINGPERFORATOR_PICKUP", "Chance to create a Malachite Urchin on enemy kill."); //Text when you hover over the item in your inventoy or the tooltip when you pick it up
+            LanguageAPI.Add("ROTTINGPERFORATOR_DESC", "<style=cIsDamage>10%</style> chance on killing an enemy to create a friendly Malachite Urchin that attacks nearby enemies. The Malachite Urchin triggers any on-kill effects when it dies <style=cIsUtility>1</style> <style=cStack>(+1 per stack)</style> times."); //Description of the item in the logbook (also I think what the tooltip is with ItemStats on?
+            LanguageAPI.Add("ROTTINGPERFORATOR_LORE", "Subject: Rotting Perforator\n\nTechnician: Dema \"Dembones\" Brown\nTable Spec: N/A\nNotes:\n\n> I am NOT touching this thing.\n> Prepare for immediate disposal, this thing is absolutely vile. Not worth experimenting on."); //Lore of the item in the logbook. \n creates a new line.
+        }
+        public static TeamIndex? teamIndex = new TeamIndex?(TeamIndex.Player);
+        private static void Hooks()
+        {
+            //Spawning Urchins
+            On.RoR2.GlobalEventManager.OnCharacterDeath += (On.RoR2.GlobalEventManager.orig_OnCharacterDeath orig, GlobalEventManager self, DamageReport damageReport) =>
+            {
+                if (damageReport?.attackerBody && damageReport?.attackerMaster)
+                {
+                    CharacterBody character = damageReport.attackerBody;
+                    if (character.inventory.GetItemCount(itemDef) > 0 && Util.CheckRoll(10f, damageReport.attackerMaster) && damageReport.victimBody.inventory.GetItemCount(Other.MalachiteUrchinOnKill.itemDef) == 0)
+                    {
+                        InputBankTest inputBankTest = null;
+                        Ray ray = inputBankTest ? inputBankTest.GetAimRay() : new Ray(damageReport.victimBody.transform.position, damageReport.victimBody.transform.rotation * Vector3.forward);
+                        Vector3 vector = damageReport.victimBody.transform.position;
+                        Vector3 position2 = vector;
+                        Quaternion rotation2 = Quaternion.LookRotation(ray.direction);
+                        GameObject gameObject3 = Object.Instantiate(Resources.Load<GameObject>("Prefabs/CharacterMasters/UrchinTurretMaster"), position2, rotation2);
+                        CharacterMaster component2 = gameObject3.GetComponent<CharacterMaster>();
+                        NetworkServer.Spawn(gameObject3);
+                        MinionOwnership.MinionGroup.SetMinionOwner(component2.minionOwnership, damageReport.attackerMaster.netId);
+                        component2.teamIndex = (TeamIndex)teamIndex;
+                        component2.inventory.GiveItem(Other.MalachiteUrchinOnKill.itemDef, character.inventory.GetItemCount(itemDef));
+                        component2.inventory.GiveItem(RoR2Content.Items.HealthDecay, 1);
+                        component2.inventory.GiveItem(RoR2Content.Items.BoostAttackSpeed, 10 * character.inventory.GetItemCount(itemDef) - 1);
+                        component2.SpawnBodyHere();
+                    }
+                }
+                orig(self, damageReport);
+            };
+
+            //Urchins proccing on kill items when they die
+            On.RoR2.CharacterBody.OnDeathStart += (orig, characterBody) =>
+            {
+                if (characterBody.master.teamIndex == teamIndex && characterBody.inventory.GetItemCount(Other.MalachiteUrchinOnKill.itemDef) > 0)
+                {
+                    if (NetworkServer.active)
+                    {
+                        for (int i = 0; i < characterBody.inventory.GetItemCount(Other.MalachiteUrchinOnKill.itemDef); i++)
+                        {
+                            DamageInfo damageInfo = new DamageInfo
+                            {
+                                attacker = characterBody.master.minionOwnership.ownerMaster.GetBodyObject(),
+                                crit = false,
+                                damage = characterBody.damage,
+                                position = characterBody.transform.position,
+                                procCoefficient = 1f,
+                                damageType = DamageType.Generic,
+                                damageColorIndex = DamageColorIndex.Default
+                            };
+                            HealthComponent victim = characterBody.healthComponent;
+                            DamageReport damageReport = new DamageReport(damageInfo, victim, damageInfo.damage, characterBody.healthComponent.combinedHealth);
+                            //characterBody.HandleOnKillEffectsServer(damageReport); idk if this is needed but it's commented out incase it appears it is needed
+                            GlobalEventManager.instance.OnCharacterDeath(damageReport);
+                        }
+                    }
+                }
+                orig(characterBody);
+            };
+        }
+    }
+}*/
---
+++ Slipstream/Assets/Slipstream/Scripts/Main.cs
@@ -0,0 +1,138 @@
+﻿/*using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using RoR2.ContentManagement;
+using Slipstream.Items.Other;
+using Slipstream.Items.White;
+using Slipstream.Items.Yellow;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Slipstream
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(ItemDropAPI), nameof(LanguageAPI), nameof(ResourcesAPI), nameof(PrefabAPI), nameof(BuffAPI), nameof(LoadoutAPI), nameof(ProjectileAPI), nameof(RecalculateStatsAPI))]
+    public class ContentPackProvider : IContentPackProvider
+    {
+        public static SerializableContentPack serializedContentPack;
+        public static ContentPack contentPack;
+        //Should be the same names as your SerializableContentPack in the asset bundle
+        public static string contentPackName = "SlipstreamContentPack";
+
+        public string identifier
+        {
+            get
+            {
+                //If I see this name while loading a mod I will make fun of you
+                return "Slipstream";
+            }
+        }
+
+        internal static void Initialize()
+        {
+            contentPack = serializedContentPack.CreateContentPack();
+            ContentManager.collectContentPackProviders += AddCustomContent;
+        }
+
+        private static void AddCustomContent(ContentManager.AddContentPackProviderDelegate addContentPackProvider)
+        {
+            addContentPackProvider(new ContentPackProvider());
+        }
+
+        public IEnumerator LoadStaticContentAsync(LoadStaticContentAsyncArgs args)
+        {
+            args.ReportProgress(1f);
+            yield break;
+        }
+
+        public IEnumerator GenerateContentPackAsync(GetContentPackAsyncArgs args)
+        {
+            ContentPack.Copy(contentPack, args.output);
+            args.ReportProgress(1f);
+            yield break;
+        }
+
+        public IEnumerator FinalizeAsync(FinalizeAsyncArgs args)
+        {
+            args.ReportProgress(1f);
+            yield break;
+        }
+    }
+
+    public static class Assets
+    {
+        public static AssetBundle mainAssetBundle = null;
+        //the filename of your assetbundle
+        internal static string assetBundleName = "SlipstreamAssets";
+
+        internal static string assemblyDir
+        {
+            get
+            {
+                return Path.GetDirectoryName(Main.Info.Location);
+            }
+        }
+
+        public static void PopulateAssets()
+        {
+            mainAssetBundle = AssetBundle.LoadFromFile(Path.Combine(assemblyDir, assetBundleName));
+            ContentPackProvider.serializedContentPack = mainAssetBundle.LoadAsset<SerializableContentPack>(ContentPackProvider.contentPackName);
+        }
+    }
+
+    public class SlipstreamPlugin : BaseUnityPlugin
+    {
+        public static List<ItemDef> ModItemDefs = new List<ItemDef>();
+
+        internal ContentPack contentPack = new ContentPack();
+
+        public static AssetBundle AssetBundle = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("Slipstream.slipstreamassets"));
+
+        private const string ModVer = "0.0.1";
+        private const string ModName = "Slipstream";
+        public const string ModGuid = "com.TeamSlipstream.Slipstream";
+        public static string MODPREFIX = "@Slipstream:";
+        public string identifier => "com.TeamSlipstream.Slipstream";
+
+        public static PluginInfo pluginInfo;
+
+        public void Awake() //Awake() is run when the mod loads upon starting your game
+        {
+            pluginInfo = this.Info;
+            //You have to call the Init() method of any items you add to here
+            #region Item Inits
+
+            //Whites
+            SnakeEyes.Init();
+            //var CowboyHatItem = new CowboyHat();
+            //CowboyHatItem.Init();
+
+            //Greens
+
+            //Reds
+
+            //Yellows
+            RottingPerforator.Init();
+            SubzeroPerforator.Init();
+
+            //Blues
+
+            //Equipment
+
+            //Other Stuff
+            MalachiteUrchinOnKill.Init();
+
+            #endregion Item Inits
+
+
+            RoR2Application.isModded = true;
+            ContentManager.collectContentPackProviders += ContentManager_collectContentPackProviders;
+            Logger.LogMessage("The Slipstream has broken.\n\nRun."); //Text that gets written to the console, this is mainly here just to make sure the mod loads successfully
+        }
+    }
+}*/
