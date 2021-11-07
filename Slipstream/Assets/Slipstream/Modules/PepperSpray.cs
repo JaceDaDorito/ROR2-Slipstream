3f128e6c787a9b47afac29a53a380b02b1dd008e
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2021-11-06 21:01:07 -0400

    update
    yea

---
+++ Slipstream/Assets/Slipstream/Modules/PepperSpray.cs
@@ -0,0 +1,13 @@
+﻿using R2API;
+using RoR2;
+using BepInEx;
+using UnityEngine;
+using static On.RoR2.HealthComponent;
+using static R2API.RecalculateStatsAPI;
+
+namespace Slipstream.Modules
+{
+    class PepperSpray
+    {
+    }
+}
