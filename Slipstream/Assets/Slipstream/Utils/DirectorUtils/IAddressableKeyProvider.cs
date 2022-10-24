1045870418254169d4c725f0ee83a6a2b5dbe2a1
Priscillalala <95263024+Priscillalala@users.noreply.github.com>
2022-10-24 10:59:13 -0700

    slip director editor, slipfamilydccs, ..

--- Slipstream/Assets/Slipstream/Utils/DirectorUtils/IAddressableKeyProvider.cs
+++ Slipstream/Assets/Slipstream/Utils/DirectorUtils/IAddressableKeyProvider.cs
@@ -13,21 +13,6 @@ using System.Collections.Generic;
 
 namespace Slipstream.Scenes
 {
-    public static class IAddressableKeyProviderExt
-    {
-        public static void Resolve<T>(this IAddressableKeyProvider<T> provider)
-        {
-            if (!string.IsNullOrEmpty(provider.Key))
-            {
-                T addressable = Addressables.LoadAssetAsync<T>(provider.Key).WaitForCompletion();
-                if (addressable == null)
-                {
-                    SlipLogger.LogW(provider + ": Addressable key [" + provider.Key + "] was provided, but returned null!");
-                }
-                provider.Addressable = addressable;
-            }
-        }
-    }
     public interface IAddressableKeyProvider<T>
     {
         string Key { get; }
