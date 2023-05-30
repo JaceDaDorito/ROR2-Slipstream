7638310d9454776ccc33dc350a4f66162139b9e6
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2023-05-30 02:41:09 -0400

    sending changes to main computer

---
+++ Slipstream/Assets/SlipstreamMod/Content/Skills/Sharpshooter/SharpshooterState.cs
@@ -0,0 +1,23 @@
+﻿using Moonstorm;
+using R2API.ScriptableObjects;
+using RoR2.ContentManagement;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using RoR2.Skills;
+using RoR2.Projectile;
+using RoR2;
+using UnityEngine.AddressableAssets;
+using HG;
+using Slipstream;
+
+namespace EntityStates.Commando.CommandoWeapon
+{
+    public class SharpshooterState : BaseState
+    {
+        private const string token = "SLIP_SKILL_SHARPSHOOTER_DESC";
+
+        
+    }
+
+}
